dc8e3d0100c05be00d2eda55c0f43948b85fe130
mahmud <mahmudhussaininfo@gmail.com>
2025-03-15 15:57:06 +0600

    controller added

---
+++ api/controller/userController.js
@@ -0,0 +1,33 @@
+import User from "../model/User.js";
+import bcrypt from "bcryptjs";
+
+// register user
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
--- api/routes/api.js
+++ api/routes/api.js
@@ -1,8 +1,10 @@
 import express from "express";
+import * as userController from "../controller/userController.js";
 // ROUTING
 const router = express.Router();
 
 // CONTROLLERS
+router.post("/register", userController.registerUser);
 
 //export
 export default router;
