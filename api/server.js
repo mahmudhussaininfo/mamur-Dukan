e42861830b90c8de46ae0ef5cf84e1f28a97c83e
mahmud <mahmudhussaininfo@gmail.com>
2025-04-06 19:29:21 +0600

    server updated

--- api/server.js
+++ api/server.js
@@ -16,7 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3030", "http://localhost:4040"],
+    origin: [
+      "http://localhost:3030",
+      "http://localhost:4040",
+      "https://mamur-dukan-client.vercel.app",
+      "https://mamur-dukan-admin.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
